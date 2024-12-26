80d815be5b62e9e5e60b3c26434624f611096f70
InHoYi <inhomike17@gmail.com>
2024-12-26 11:55:36 +0900

    중복 제거 함수 및 원핫인코딩 함수 추가

--- bow.py
+++ bow.py
@@ -1,15 +1,42 @@
 import numpy as np
 
+def deleteDuplicates(aListofWords: list) -> list:
+    result = []
+    for word in aListofWords:
+        if word not in result:
+            result.append(word)
+    
+    return result
+
+
+def deleteDuplicatesbySet(aListofWords: list) -> list:
+    result = set(aListofWords)
+
+    return result
+
+
 def wordToNumberDict(aListofWords: list) -> dict:
     result = {}
     for word in aListofWords:
         if word not in result:
             result[word] = len(result) + 1
+
     return result
+
 
 def numberToWordDict(aListofWords: list) -> dict:
     result = {}
     for word in aListofWords:
         if word not in result.values():
             result[len(result) + 1] = word
+
+    return result
+
+
+def oneHotEncoding(aListofWords: list) -> dict:
+    result = {}
+    uniqueSet = deleteDuplicatesbySet(aListofWords)
+    values = np.eye(len(uniqueSet))
+    result = dict(zip(uniqueSet, values))
+    
     return result
