b5e761df15465d63bd4deed1c60359d703669c25
InHoYi <inhomike17@gmail.com>
2024-12-26 11:41:21 +0900

    numberToWordDict 함수 추가

--- bow.py
+++ bow.py
@@ -6,3 +6,10 @@ def wordToNumberDict(aListofWords: list) -> dict:
         if word not in result:
             result[word] = len(result) + 1
     return result
+
+def numberToWordDict(aListofWords: list) -> dict:
+    result = {}
+    for word in aListofWords:
+        if word not in result.values():
+            result[len(result) + 1] = word
+    return result
