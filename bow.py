57cdb2ac9146bb22e4713d60bd5af5f2b5492023
InHoYi <inhomike17@gmail.com>
2024-12-26 11:22:32 +0900

    wordListTodict 함수 완성

--- bow.py
+++ bow.py
@@ -1,2 +1,8 @@
 import numpy as np
 
+def wordListTodict(aListofWords: list) -> dict:
+    result = {}
+    for word in aListofWords:
+        if word not in result:
+            result[word] = len(result) + 1
+    return result
